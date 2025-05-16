020f286bc0a0437c3adb3e958c1bf48fefbeb6a4
Mike Costa <mike.csk@hotmail.com>
2025-05-16 02:46:01 +0000

    VersãodeProdução

--- Xadrez.c
+++ Xadrez.c
@@ -7,7 +7,6 @@ void moverTorre(int casas){
     }
 }
     
-        
 void moverRainha(int movimentoRainha){
     
     if (movimentoRainha > 0)
@@ -15,33 +14,40 @@ void moverRainha(int movimentoRainha){
         printf("Esquerda \n");
         moverRainha(movimentoRainha - 1);
     }
-    
 }
     
-
 int main(){
 
-    //VARIAVEL TORRE DEFINIDA
-    int torre, rainha = 0, bispo = 0, cavalo = 0;
-
     //Mover a torre 5 casas para a direita
+    printf("Movimento Torre: \n");
     moverTorre(5);
     
     //mover a rainha 8 casas para esquerda
+    printf(" \n");
+    printf("Movimento Rainha: \n");
     moverRainha(8);
 
    //movimenta o bispo 5 casas para direita cima
-for(bispo < 5; bispo++){
+for(int bispo = 0;bispo < 1; bispo++){
+    printf(" \n");
+    printf("Movimento Bispo: \n");
     printf("Cima\n");
-    for (int movimentoBispo = 0; movimentoBispo < 5; movimentoBispo++){
-    printf("Direita \n");
+    printf("Cima\n");
+    printf("Cima\n");
+    printf("Cima\n");
+    printf("Cima\n");
+    for(int movimentoBispo = 0; movimentoBispo < 5; movimentoBispo++){
+        printf("Direita \n");
     }
+    
 } 
 
 //movimentação do cavalo com loop aninhado mover para cima e direita
 for(int cavalo = 0, movimentoCav = 1; cavalo < movimentoCav ; cavalo++ )
-{
-    printf("Cima, Cima\n");
+    printf(" \n");
+{   printf("Movimento Cavalo: \n");
+    printf("Cima\n");
+    printf("Cima\n");    
 
     for (int movimentoLateral = 0; movimentoLateral < 1; movimentoLateral++)
     {
